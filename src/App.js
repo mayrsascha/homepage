ac51e411e60e299c571f3b0c0439d00996c38ddd
mayrsascha <mayr.sascha@gmail.com>
2020-07-15 04:38:10 +0700

    Add alt to the image profile photo

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <main className="content">
         <Hero/>
-        <img className="profile" src="/profile.png"></img>
+        <img className="profile" src="/profile.png" alt="Profile photo"></img>
       </main>
       <Footer/>
     </div>
