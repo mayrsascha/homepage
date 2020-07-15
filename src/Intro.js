17a3b6800c316241de5073208679dd5d5ca333d1
Sascha Mayr <mayr.sascha@gmail.com>
2020-07-15 14:55:52 +0200

    Add  rel="noopener noreferrer" to all external links

--- src/Intro.js
+++ src/Intro.js
@@ -11,12 +11,12 @@ More about me at the links below.
 - <a href="https://stackoverflow.com/users/story/1864315" target="_blank" rel="noopener noreferrer">[Stack Overflow](https://stackoverflow.com/users/story/1864315)</a>
 
 <h2>## Personal profiles</h2>
-- <a href="https://github.com/mayrsascha">[GitHub](https://github.com/mayrsascha)</a>
-- <a href="https://twitter.com/realsidehacker">[Twitter](https://twitter.com/realsidehacker)</a>
-- <a href="https://www.instagram.com/sascha.lives">[Instagram](https://www.instagram.com/sascha.lives)</a>
+- <a href="https://github.com/mayrsascha" rel="noopener noreferrer">[GitHub](https://github.com/mayrsascha)</a>
+- <a href="https://twitter.com/realsidehacker" rel="noopener noreferrer">[Twitter](https://twitter.com/realsidehacker)</a>
+- <a href="https://www.instagram.com/sascha.lives" rel="noopener noreferrer">[Instagram](https://www.instagram.com/sascha.lives)</a>
 - <a href="mailto:mayr.sascha@gmail.com">[Email](mailto:mayr.sascha@gmail.com)</a>
 
 <h2>## Pet projects</h2>
-- <a href="https://www.iswatersafetodrink.in">[Is water safe to drink in...?](https://www.iswatersafetodrink.in)</a>
-- <a href="https://cutt.ly/Ep8dfug">[Incognito killer Chrome extension](https://cutt.ly/Ep8dfug)</a>
+- <a href="https://www.iswatersafetodrink.in" rel="noopener noreferrer">[Is water safe to drink in...?](https://www.iswatersafetodrink.in)</a>
+- <a href="https://cutt.ly/Ep8dfug" rel="noopener noreferrer">[Incognito killer Chrome extension](https://cutt.ly/Ep8dfug)</a>
 `
