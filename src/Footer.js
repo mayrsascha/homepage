de9a6d624be0e8a15b872b44fd4a930b3b759f48
Sascha Mayr <mayr.sascha@gmail.com>
2020-07-15 23:16:14 +0200

    Add link to the GitHub repo

--- src/Footer.js
+++ src/Footer.js
@@ -11,6 +11,9 @@ function Footer(props) {
       <button className="button" onClick={() => {props.darkMode ? props.setDarkMode(false) : props.setDarkMode(true)} }>
         { props.darkMode ? "🌞" : "🌙" }
       </button>
+      <a href="https://github.com/mayrsascha/homepage" target="_blank" rel="noopener noreferrer" className="github-link">
+        <img src={props.darkMode ? "GitHub-Mark-64px-Light.png" : "GitHub-Mark-64px.png"} alt="github" />
+      </a>
     </footer>
   );
 }
