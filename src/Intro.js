cf2f0ab6deaddccbadc8d3141ee7b91d431cd559
Sascha Mayr <mayr.sascha@gmail.com>
2020-07-16 17:06:50 +0200

    Shorten links

--- src/Intro.js
+++ src/Intro.js
@@ -7,16 +7,16 @@ Drinking too much coffee <span role="img" aria-label="coffe icon">☕️</span>.
 More about me at the links below.
 
 <h2>## Professional profiles</h2>
-- <a href="https://www.toptal.com/resume/sascha-mayr" target="_blank" rel="noopener noreferrer">[TopTal](https://www.toptal.com/resume/sascha-mayr)</a>
-- <a href="https://stackoverflow.com/users/story/1864315" target="_blank" rel="noopener noreferrer">[Stack Overflow](https://stackoverflow.com/users/story/1864315)</a>
+- <a href="https://cutt.ly/iarIG3p" target="_blank" rel="noopener noreferrer">[TopTal](https://cutt.ly/iarIG3p)</a>
+- <a href="https://cutt.ly/narCNb4" target="_blank" rel="noopener noreferrer">[Stack Overflow](https://cutt.ly/narCNb4)</a>
 
 <h2>## Personal profiles</h2>
-- <a href="https://github.com/mayrsascha" target="_blank" rel="noopener noreferrer">[GitHub](https://github.com/mayrsascha)</a>
-- <a href="https://twitter.com/realsidehacker" target="_blank" rel="noopener noreferrer">[Twitter](https://twitter.com/realsidehacker)</a>
-- <a href="https://www.instagram.com/sascha.lives" target="_blank" rel="noopener noreferrer">[Instagram](https://www.instagram.com/sascha.lives)</a>
+- <a href="https://cutt.ly/xarC2mt" target="_blank" rel="noopener noreferrer">[GitHub](https://cutt.ly/xarC2mt)</a>
+- <a href="https://cutt.ly/IarVG4E" target="_blank" rel="noopener noreferrer">[Twitter](https://cutt.ly/IarVG4E)</a>
+- <a href="https://cutt.ly/YarC6s5" target="_blank" rel="noopener noreferrer">[Instagram](https://cutt.ly/YarC6s5)</a>
 - <a href="mailto:mayr.sascha@gmail.com">[Email](mailto:mayr.sascha@gmail.com)</a>
 
 <h2>## Pet projects</h2>
-- <a href="https://www.iswatersafetodrink.in" target="_blank" rel="noopener noreferrer">[Is water safe to drink in...?](https://www.iswatersafetodrink.in)</a>
+- <a href="https://cutt.ly/1arVt8y" target="_blank" rel="noopener noreferrer">[Is water safe to drink in...?](https://cutt.ly/1arVt8y)</a>
 - <a href="https://cutt.ly/Ep8dfug" target="_blank" rel="noopener noreferrer">[Incognito killer Chrome extension](https://cutt.ly/Ep8dfug)</a>
 `
